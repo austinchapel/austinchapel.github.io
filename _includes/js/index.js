d72a16c19cad62c9179b142d4490105b4ad46528
Aaron Stacy <aaron.r.stacy@gmail.com>
2015-07-19 22:33:31 -0500

    added new mobile nav

--- _includes/js/index.js
+++ _includes/js/index.js
@@ -4,30 +4,26 @@
   var NAV_HEIGHT = $nav.height();
   var vh = $window.height();
 
-  state({
-    vh: $window.height(),
-    scroll: window.scrollY,
-    fixed: false
-  });
-
   function navShouldBeFixed() {
     return state().scroll >= state().vh - NAV_HEIGHT;
   }
 
   function fixNav(shouldFix) {
-    if (shouldFix)
+    if (shouldFix) {
+      $nav.find('.nav-menu').addClass('no-transition');
       $nav.addClass('fixed');
-    else
+      setTimeout(function() {
+        $nav.find('.nav-menu').removeClass('no-transition');
+      }, 0);
+    } else {
+      $nav.find('.nav-menu').addClass('no-transition');
       $nav.removeClass('fixed');
+      $nav.find('.nav-menu')[0].getBoundingClientRect();
+      setTimeout(function() {
+        $nav.find('.nav-menu').removeClass('no-transition');
+      }, 0);
+    }
   }
-
-  $(window).on('resize orientationchange', function() {
-    state({vh: $window.height()});
-  });
-
-  $(window).on('scroll', function() {
-    state({scroll: window.scrollY});
-  });
 
   state.onchange(function(event, current, previous) {
     if (current.scroll != previous.scroll || current.vh != previous.vh)
@@ -35,6 +31,27 @@
 
     if (current.fixed != previous.fixed)
       fixNav(current.fixed);
+
+    if (current.navOpen !== previous.navOpen)
+     if (current.navOpen)
+       $nav.addClass('open');
+     else
+       $nav.removeClass('open');
   });
 
+  $window.on('resize orientationchange', function() {
+      state({vh: $window.height()});
+    })
+    .on('scroll', function() {
+      state({scroll: window.scrollY});
+    });
+
+  $(document.body).on('click', '.hamburger', function() {
+    state({navOpen: !state().navOpen});
+  });
+
+  state({
+    vh: $window.height(),
+    scroll: window.scrollY
+  });
 })();
